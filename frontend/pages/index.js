844cbfd78e212647439f8179c69f40e48c708ef1
yash621 <yg17381@gmail.com>
2022-01-24 22:28:48 +0530

    configured tailwind,updated titile and logo,cleaned bolerplate

---
+++ frontend/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>metameet.io</title>
+        <link rel="icon" href="/static/images/meta-meet-logo.jpg" />
+      </Head>
+
+      <main className={styles.main}>
+       <h1 className="text-3xl font-bold underline">meta meet</h1>
+      </main>
+      <footer className={styles.footer}>
+      </footer>
+    </div>
+  )
+}
