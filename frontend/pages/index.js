650aa3212d602f0100084167791b7e86f5971880
yash621 <yg17381@gmail.com>
2022-01-26 13:23:01 +0530

    fixed next js dependency problems

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -10,6 +9,7 @@ export default function Home() {
         <link rel="icon" href="/static/images/meta-meet-logo.jpg" />
       </Head>
       <main className={styles.main}>
+       <h1 className="text-3xl font-bold underline">meta meet</h1>
        <h1 className="text-3xl font-bold underline">meta meet</h1>
       </main>
       <footer className={styles.footer}>
