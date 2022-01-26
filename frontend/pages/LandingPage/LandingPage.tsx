2b9e6db1197b8c7b6fe883f11fcfa1912991fff1
yash621 <yg17381@gmail.com>
2022-01-27 02:12:19 +0530

    removed tailwing configuration and updated logo

---
+++ frontend/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LandingPageCSS from "./LandingPage.module.css";
+import Logo from "../../public/static/images/logo.png";
+import Image from 'next/image'
+
+function LandingPage() {
+  
+  return <div className={LandingPageCSS.landingPage}>
+    <Image src={Logo} alt="welcome to meta-meet.io" className={LandingPageCSS.logo} />
+    </div>;
+}
+
+export default LandingPage;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,19 +1,15 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import LandingPage from "./LandingPage/LandingPage.tsx";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>metameet.io</title>
-        <link rel="icon" href="/static/images/meta-meet-logo.jpg" />
+        <link rel="icon" href="/static/images/logo.png" />
       </Head>
-      <main className={styles.main}>
-       <h1 className="text-3xl font-bold underline">meta meet</h1>
-       <h1 className="text-3xl font-bold underline">meta meet</h1>
-      </main>
-      <footer className={styles.footer}>
-      </footer>
+      <LandingPage />
     </div>
-  )
+  );
 }
