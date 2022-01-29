e587e3babf51a9c73bbba819d63476d8b8a4d109
yash621 <yg17381@gmail.com>
2022-01-29 23:42:32 +0530

    firebase setup completed

--- frontend/components/Login/Login.tsx
+++ frontend/components/Login/Login.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import LoginCSS from "../Login/Login.module.css";
 import Image from "next/image";
 import Logo from "../../public/static/images/logo.png";
+import { auth, provider } from "../../firebase";
 
 function Login({ authState }) {
+  const signIn = () => {
+    auth.signInWithPopup(provider).catch(alert);
+  };
+
   return (
     <div className={LoginCSS.login}>
       <div className={LoginCSS.companyName}>
@@ -34,7 +39,7 @@ function Login({ authState }) {
         <button className={LoginCSS.submitLogin}>{authState}</button>
         <div className={LoginCSS.forgotPass}>
           {authState === "Sign In" && <p>Forgot Password?</p>}
-          <p>{authState} with Google</p>
+          <p onClick={signIn}>{authState} with Google</p>
           {authState !== "Sign In" && <p>Already have an account ? Sign In</p>}
         </div>
       </div>
