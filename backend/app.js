f3f120eaf2d2a661d15b3cb06cb804bd19372f0d
yash621 <yg17381@gmail.com>
2022-03-16 19:36:34 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,7 @@ const app = express();
 require("dotenv").config();
 const server = http.createServer(app);
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://yash621:HOkYE3VwcfY0IdRy@cluster0.jnqnm.mongodb.net/meta_meet?retryWrites=true&w=majority",
-  { useNewUrlParser: true }
-);
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
