5547604436f4f71d977dad08db895e7794410297
yash621 <yg17381@gmail.com>
2022-09-06 21:51:27 +0530

    updated port messages

--- backend/app.js
+++ backend/app.js
@@ -106,4 +106,4 @@ app.get("/", (req, res) => {
 });
 
 const port = process.env.PORT || 9000;
-server.listen(port, () => console.log("server is running on port 5000"));
+server.listen(port, () => console.log(`server is running on port ${port}`));
